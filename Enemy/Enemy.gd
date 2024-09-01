81fe17e205f713dae3424ef6163842b401d05e5b
Dante Federici <c.dante.federici@gmail.com>
2024-09-01 13:16:45 -0400

    Enemies move toward the player

--- Enemy/Enemy.gd
+++ Enemy/Enemy.gd
@@ -1,8 +1,15 @@
 class_name Enemy
 extends CharacterBody2D
 
+@export var target: Node2D
+@onready var _sprite_move = $SpriteMove
+
 func _physics_process(delta):
-	pass
+	if target:
+		_sprite_move.input_vector = Vector2.from_angle(get_angle_to(target.position))
+	
+	_sprite_move.tick(delta)
+	move_and_slide()
 
 func _on_area_2d_body_entered(body):
 	print(body)
