d183f3bcd5d358f7a6d42c861a4861f5a2211f12
Dante Federici <c.dante.federici@gmail.com>
2024-09-02 00:45:33 -0400

    Death, explosions, oh my!

--- TestArena/TestArena.gd
+++ TestArena/TestArena.gd
@@ -1,10 +1,21 @@
 extends Node2D
 
 const EnemyScene: PackedScene = preload("res://Enemy/Enemy.tscn")
+const ExplosionScene: PackedScene = preload("res://Explosion/Explosion.tscn")
 
 @onready var game: CanvasLayer = $Game
 @onready var player: Player = $Game/Player
 @onready var arena_area: CollisionShape2D = $Game/AreanaArea/CollisionShape2D
+
+var particle_pool = []
+func explode(pos: Vector2):
+	var explosion: Explosion = particle_pool.pop_front()
+	if explosion == null:
+		explosion = ExplosionScene.instantiate()
+		explosion.finished.connect(func (): particle_pool.push_back(explosion))
+		game.add_child(explosion)
+	
+	explosion.fire_at(pos)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -18,8 +29,12 @@ func _add_enemey(point: Vector2):
 	var enemy: Enemy = EnemyScene.instantiate()
 	enemy.target = player
 	enemy.position = point
-	#var health: Health = enemy.get_node("Health")
-	#health.on_death.connect(func (taget: Node2D, killer: Node2D): print("DEAD! ", a, ", ", b, " ... ", health.owner, "...", b.owner))
+	var health: Health = enemy.get_node("Health")
+	health.on_death.connect(
+		func (target: Node2D, killer: Node2D):
+			print(target, " killed by: ", killer)
+			explode(target.global_position)
+	)
 	game.add_child(enemy)
 
 func _pt_in_rect(rect: Rect2, margin: float = 1.0) -> Vector2:
